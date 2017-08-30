4397395d2dd915b30faa54daef2a47ef6d9cd27b
SauceSoy <ahmedsiad114@gmail.com>
2017-08-29 21:19:46 -0600

    fix

--- src/commands/crime/Rob.js
+++ src/commands/crime/Rob.js
@@ -43,6 +43,8 @@ class Rob extends patron.Command {
 
       return msg.createReply('Whilst your target was at the bank, you disguised yourself as a clerk and offered to help him with his deposit. Luckily, you managed to snipe some cash from his account and the dude walked away without noticing. You robbed ' + args.resources.USD() + '.');
     }
+    await db.userRepo.modifyCash(msg.dbGuild, msg.member, -args.resources);
+
     return msg.createReply('You snuck up behind the dude at the bank and surprised his fatass. Unfortunately for you, he was a SWAT agent and sent you to jail. You lost all the resources in the process.');
   }
 }
